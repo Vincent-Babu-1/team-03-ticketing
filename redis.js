bed760df2dfcef872e9dfca9ff8a943d4c906e39
Rihui Lu <rihuilu1108@gmail.com>
2026-04-09 14:57:05 -0400

    added caching logic to redis

--- redis.js
+++ redis.js
@@ -1,4 +1,4 @@
-import Redis from "ioredis";
+import Redis from "redis";
 
 // ---------- Connections ----------
 export const redis = new Redis(process.env.REDIS_URL);          // general use / queues
@@ -18,3 +18,13 @@ export const popFromQueue = async (queue) => {
 // ---------- Pub/Sub ----------
 export const publish = (channel, message) =>
   redisPublisher.publish(channel, JSON.stringify(message));
+
+// ---------- Cache ----------
+export const setCache = async (key, value, ttlSeconds = 300) => {
+  await redis.set(key, JSON.stringify(value), "EX", ttlSeconds);
+};
+
+export const getCache = async (key) => {
+  const data = await redis.get(key);
+  return data ? JSON.parse(data) : null;
+};
