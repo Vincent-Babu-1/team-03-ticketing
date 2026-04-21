fe6ddfc200342dc1cf666b9dbd0a34be578ace03
Rihui Lu <rihuilu1108@gmail.com>
2026-04-21 11:45:06 -0400

    moved worker functions to service functions and added publish functionality to notification worker

--- C-notification/notif-service.js
+++ C-notification/notif-service.js
@@ -17,6 +17,51 @@ app.get('/health', async (req, res) => {
   }
 });
 
+const CHANNEL = "confirmed-purchases";
+
+async function startWorker() {
+  console.log("Notification worker starting...");
+
+
+  // 2. subscribe AFTER connection
+  await redisSubscriber.subscribe(CHANNEL, (message) => {
+    try {
+      const data = JSON.parse(message);
+
+      console.log("Received confirmed purchase:");
+      console.log(data);
+
+      // simulate email sending
+      const emailLog = `
+        === EMAIL SENT ===
+        To: ${data.email}
+        Subject: Purchase Confirmation
+        Body: Your purchase of ${data.item} is confirmed.
+        ==================
+        `;
+
+      console.log(emailLog);
+
+      // optional file log
+      // fs.appendFileSync("emails.log", emailLog);
+
+    } catch (err) {
+      console.error("Failed to process message:", err);
+    }
+  });
+
+  console.log(`Listening on ${CHANNEL}...`);
+
+  // keep process alive (important for Docker)
+  process.stdin.resume();
+}
+
+// startup safety
+startWorker().catch((err) => {
+  console.error("🔥 Worker failed to start:", err);
+  process.exit(1);
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Refund service listening on :${port}`);
--- C-notification/notif-worker.js
+++ C-notification/notif-worker.js
@@ -1,47 +1,9 @@
 import { redisSubscriber } from "./redis.js";
 
 console.log("WORKER FILE STARTED");
-const CHANNEL = "confirmed-purchases";
 
-async function startWorker() {
-  console.log("Notification worker starting...");
+const purchaseId = "abc123";
+const userId = "user456";
+const eventId = "event789";
 
-
-  // 2. subscribe AFTER connection
-  await redisSubscriber.subscribe(CHANNEL, (message) => {
-    try {
-      const data = JSON.parse(message);
-
-      console.log("Received confirmed purchase:");
-      console.log(data);
-
-      // simulate email sending
-      const emailLog = `
-        === EMAIL SENT ===
-        To: ${data.email}
-        Subject: Purchase Confirmation
-        Body: Your purchase of ${data.item} is confirmed.
-        ==================
-        `;
-
-      console.log(emailLog);
-
-      // optional file log
-      // fs.appendFileSync("emails.log", emailLog);
-
-    } catch (err) {
-      console.error("Failed to process message:", err);
-    }
-  });
-
-  console.log(`Listening on ${CHANNEL}...`);
-
-  // keep process alive (important for Docker)
-  process.stdin.resume();
-}
-
-// startup safety
-startWorker().catch((err) => {
-  console.error("🔥 Worker failed to start:", err);
-  process.exit(1);
-});
+await redis.publish('confirmed-purchases', JSON.stringify({ purchaseId, userId, eventId }));
